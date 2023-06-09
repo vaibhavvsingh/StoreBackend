63e107146d8b07ff46daefeea20e047c7bdd986e
Vaibhav Singh <89960983+vaibhavvsingh@users.noreply.github.com>
2023-06-09 10:52:37 +0530

    Added environment variables

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
